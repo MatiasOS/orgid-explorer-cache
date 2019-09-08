5361fc0ce3afd7ede795659c4c693c2862183890
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-09-08 11:12:19 -0300

    chore(prod): Allow to change config with .env

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,14 +1,14 @@
 const knex = require('knex');
 
 module.exports = {
-  baseUrl: 'https://explorer.myorgid.com',
+  baseUrl: 'https://explorer-cache.myorgid.com',
   db: knex({
     client: 'pg',
     connection: {
-      host: 'orgid-explorer-cache-db.ccv2mtbtm9st.eu-west-1.rds.amazonaws.com',
-      user: 'postgres',
+      host: process.env.DB_HOST || 'orgid-explorer-cache-db.ccv2mtbtm9st.eu-west-1.rds.amazonaws.com',
+      user: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD,
-      database: 'orgid-explorer-cache-db',
+      database: process.env.DB_NAME || 'orgid-explorer-cache-db',
     },
     useNullAsDefault: true,
   }),
